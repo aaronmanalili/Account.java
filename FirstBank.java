33be9ef3e01709cae1740a041693fbedd275e67b
Aaron Manalili <manalilia2@montclair.edu>
2024-01-21 20:26:11 -0500

    Create FirstBank.java

---
+++ FirstBank.java
@@ -0,0 +1,14 @@
+package firstbank;
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Account Aaron = new Account("Aaron Manalili", "A001");
+		Aaron.showMenu();
+		
+		// Project created 8/7/2021
+	}
+
+}
